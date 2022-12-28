a4b461511764f4421632c03f7781f9a6bf102dfb
Raphael Laurentino da Silva <raphael.tw22@gmail.com>
2022-12-27 22:08:34 -0300

    manutenção

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5d6b3c23437a646889ea6023178122bccc1f1762',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,27 +11,27 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5d6b3c23437a646889ea6023178122bccc1f1762',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'twbs/bootstrap' => array(
-            'pretty_version' => 'v5.2.2',
-            'version' => '5.2.2.0',
-            'reference' => '961d5ff9844372a4e294980c667bbe7e0651cdeb',
+            'pretty_version' => 'v5.2.3',
+            'version' => '5.2.3.0',
+            'reference' => 'cb021439c683d9805e2864c58095b92d405e9b11',
             'type' => 'library',
             'install_path' => __DIR__ . '/../twbs/bootstrap',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'twbs/bootstrap-icons' => array(
-            'pretty_version' => 'v1.9.1',
-            'version' => '1.9.1.0',
-            'reference' => 'f7f6f61194419dcf4447dbe05b78a72601646490',
+            'pretty_version' => 'v1.10.2',
+            'version' => '1.10.2.0',
+            'reference' => '4deaf1f69ccbe0a996c08a308525a26b05467242',
             'type' => 'library',
             'install_path' => __DIR__ . '/../twbs/bootstrap-icons',
             'aliases' => array(),
@@ -40,7 +40,7 @@
         'twitter/bootstrap' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => 'v5.2.2',
+                0 => 'v5.2.3',
             ),
         ),
     ),
